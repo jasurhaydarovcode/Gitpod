b854fb1da8c7ff05f635ce2883eabb494be0f128
jasurhaydarovcode <jasurhaydarovcode@gmail.com>
2024-07-23 01:13:04 +0500

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+const jasur = () => {
+  return {
+    name: 'my-plugin',
+    configureServer(server) {
+      server.httpServer.once('listening', () => {
+        console.log('Happy Hacking, 🫠 🫠 😴\n Github https://github.com/jasurhaydarovcode/gitpod');
+      });
+    },
+  };
+};
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), jasur()],
+  server: {
+    open: true,
+    port: 8080,
+  }
 })
